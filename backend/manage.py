5f0ceb4c363bc2f6583ade9b8e8c234b040f522e
davypythonweb <galaxys3tab32@gmail.com>
2024-11-03 23:56:57 -0300

    update configs de build React and port of the server Django

--- backend/manage.py
+++ backend/manage.py
@@ -7,6 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "7500"
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
